69f5e9fdcf310c18acc06a9e194bf99c4d3ceb72
Ulises Castro <ulisescastro2015@gmail.com>
2021-09-21 14:05:33 -0500

    Installed styled-components, added types for todo reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
+interface TaskBaseProps {
+  title: String,
+  body: String,
+  date: Date, 
+}
+
+interface TaskProps extends TaskBaseProps {
+  subtasks: TaskBaseProps[]
+}
+ 
+interface TodoReducerProps {
+  todos: TaskProps[]
+}
+  
+// Issue here, Idk why but it has issues about the type
+const initialTodoState: TodoReducerProps = {
+  todos: []
+} 
+
+const todoReducer = (state: TodoReducerProps, action: { payload: string | number, type: string })  =>{
+  switch (action.type) {
+    case 'ADD_TODO': 
+      return state;
+    case 'REMOVE_TODO':
+      return state;
+    default:
+      throw Error()
+  }
+}
+
+
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialTodoState)
+
   return (
     <div className="App">
       <header className="App-header">
