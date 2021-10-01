a6b5d362862bb117babbc8a842fa4c29232f28b1
Ulises Castro <ulisescastro2015@gmail.com>
2021-09-30 19:53:16 -0500

    Added mexican skull image and several background pics

--- src/containers/TodoBoard/styled/index.ts
+++ src/containers/TodoBoard/styled/index.ts
@@ -1,7 +1,33 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 import { InvertedBtn } from "components/Button";
 
 export const TodoBody = styled.div`
+  --bg_1: url("skull.svg") calc(100% - 55px) / 50px no-repeat;
+  --bg_2: url("skull.svg") top 10px right 10px / 50px no-repeat;
+  --bg_3: url("skull.svg") bottom 10px right 10px / 50px no-repeat;
+  --bg_4: url("skull.svg") bottom 10px left 10px / 50px no-repeat;
+
+  background: ${() => {
+    let back = "";
+    const randomX = [20, 30, 50];
+    for (let i = 1; i < 25; i++) {
+      for (let j = 1; j < 25; j++) {
+        const ifEvenPlus = !(i % 2) ? 200 : 1;
+
+        back += `url("skull.svg") calc(100% - ${10 * i * 10 - ifEvenPlus}px) calc(100% - ${
+          ( 50 * j ) - ifEvenPlus 
+        }px) / 50px no-repeat,`;
+        // back += `url("skull.svg") calc(100% - ${15 * i * 10}px) calc(100% - ${
+        //   10 * i * 25
+        // }px) / 50px no-repeat,`;
+        // back += `url("skull.svg") calc(100% - ${1 * i * 10}px) calc(100% - ${
+        //   10 * i * 25
+        // }px) / 50px no-repeat,`;
+      }
+    }
+
+    return back.slice(0, -1);
+  }};
   display: grid;
   background-color: #344fa1;
   justify-content: center;
@@ -35,4 +61,3 @@ export const TodoStatusContainer = styled.section<ITodoStatusContainer>`
 `;
 
 export const SimpleBtn = styled(InvertedBtn)``;
- 
